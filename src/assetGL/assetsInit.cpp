bf53a97a93406a71499c095e39cee098bfef4a06
griffin-lora <griffin-lora@github.com>
2024-05-07 20:58:47 -0400

    add mipmaps

--- src/assetGL/assetsInit.cpp
+++ src/assetGL/assetsInit.cpp
@@ -2,6 +2,7 @@
 #include "glHelpers/shader/shader.hpp"
 #include "render/chunkRender.hpp"
 
+#include <GL/gl.h>
 #include <glm/ext/matrix_clip_space.hpp>
 #include <glm/ext/matrix_transform.hpp>
 #include <glm/glm.hpp>
@@ -35,12 +36,13 @@ void initRenderAssets(const RenderAssetsInitInfo& info) {
     glGenTextures(1, &blockTextureArray);
 
     glBindTexture(GL_TEXTURE_2D_ARRAY, blockTextureArray);
-    glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-
-    glBindTexture(GL_TEXTURE_2D_ARRAY, blockTextureArray);
 
     // Allocate the storage && Upload pixel data.
-    glTexStorage3D(GL_TEXTURE_2D_ARRAY, 1, GL_RGBA8, 16, 16, info.numBlockTextureArrayLayers);
+    glTexStorage3D(GL_TEXTURE_2D_ARRAY, ((GLuint)floorf(log2f(16.0f))) + 1, GL_RGBA8, 16, 16, info.numBlockTextureArrayLayers);
     glTexSubImage3D(GL_TEXTURE_2D_ARRAY, 0, 0, 0, 0, 16, 16, info.numBlockTextureArrayLayers, GL_RGBA, GL_UNSIGNED_BYTE, info.blockTextureArrayImagePixels.data());
+
+    glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+
+    glGenerateMipmap(GL_TEXTURE_2D_ARRAY);
 }
